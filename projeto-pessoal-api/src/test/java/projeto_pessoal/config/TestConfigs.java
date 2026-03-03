7c3749ac13959e0b4bb639fee6ec0690e12371c3
fco-romario <fco.romario.alves@gmail.com>
2026-03-02 21:11:46 -0300

    feat: teste de Integração com container

---
+++ projeto-pessoal-api/src/test/java/projeto_pessoal/config/TestConfigs.java
@@ -0,0 +1,5 @@
+package projeto_pessoal.config;
+
+public interface TestConfigs {
+    int SERVER_PORT = 8888;
+}
---
+++ projeto-pessoal-api/src/test/java/projeto_pessoal/integratioTests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,29 @@
+package projeto_pessoal.integratioTests.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import projeto_pessoal.config.TestConfigs;
+import projeto_pessoal.integratioTests.containers.AbstractIntegrationTest;
+
+import static io.restassured.RestAssured.given;
+import static junit.framework.TestCase.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void shouldDisplaySwaggerUIPage() {
+        var content = given()
+                .basePath("/swagger-ui/index.html")
+                .port(TestConfigs.SERVER_PORT)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        assertTrue(content.contains("Swagger UI"));
+    }
+}
